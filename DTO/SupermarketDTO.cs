9f53a226232288d5c4d1abc3b50969da4a26de92
Patricia Anghelache <panghelache@talentingsoftware.com>
2023-11-21 15:17:59 +0200

    Modified the supermarkets with local images

--- DTO/SupermarketDTO.cs
+++ DTO/SupermarketDTO.cs
@@ -18,16 +18,15 @@ namespace DTO
             {
                 if (Name == "Lidl")
                 {
-                    return "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Lidl-Logo.svg/2048px-Lidl-Logo.svg.png";
-                   // return "lidl.png";
+                    return "lidl.png";
                 }
                 else if (Name == "Carrefour")
                 {
-                    return "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Carrefour_logo.svg/2554px-Carrefour_logo.svg.png";
+                    return "carrefour.png";
                 }
                 else 
                 {
-                    return "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Kaufland_201x_logo.svg/2048px-Kaufland_201x_logo.svg.png";
+                    return "kaufland.png";
                 }
             }
         }
