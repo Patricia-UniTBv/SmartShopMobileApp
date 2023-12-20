8c0d5d339b088cc3674e63d1af1a12166af51bce
Patricia Anghelache <panghelache@talentingsoftware.com>
2023-12-20 11:12:31 +0200

    Added a unit test

---
+++ SmartShopMobileApp.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using API.Controllers;
+using API.Repository;
+using API.Repository.Interfaces;
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace SmartShopMobileApp.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public async Task GetAllSupermarkets_ReturnsOkResult()
+        {
+            // Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>(); 
+            var supermarketRepositoryMock = new Mock<ISupermarketRepository>(); 
+
+            unitOfWorkMock.Setup(uow => uow.SupermarketRepository.GetAllSupermarkets())
+                          .ReturnsAsync(new List<SupermarketDTO>() { });
+
+            var controller = new SupermarketController(unitOfWorkMock.Object);
+
+            // Act
+            var result = await controller.GetAllSupermarkets();
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+            Assert.IsInstanceOf<List<SupermarketDTO>>(okResult.Value);
+        }
+
+    }
+}
