409b5b06da67fb04badbca9609ae9d9ac1dac6c8
Patricia Anghelache <panghelache@talentingsoftware.com>
2024-02-20 21:29:30 +0200

    Small fix

--- SmartShopMobileApp/Views/MapView.xaml.cs
+++ SmartShopMobileApp/Views/MapView.xaml.cs
@@ -1,12 +1,11 @@
 namespace SmartShopMobileApp.Views;
 
-//using Map = Microsoft.Maui.Controls.Maps.Map;
+
 public partial class MapView : ContentPage
 {
 	public MapView()
 	{
 		InitializeComponent();
-        //Map map = new Map();
-        //Content = map;
+        
     }
 }
